2aa57341681c63637b1fcd3044daa20ed940d94c
Arthur Berlofa Bosi <68667433+BerlofaBosi@users.noreply.github.com>
2025-03-26 21:51:00 -0300

    Add files via upload

---
+++ aula2_2.py
@@ -0,0 +1,10 @@
+dia = 1
+hora = 3
+minutos = 46
+segundos = 40
+
+hora += dia * 24
+minutos += hora * 60
+segundos += minutos * 60
+
+print(segundos)
---
+++ aula2_4.py
@@ -0,0 +1,6 @@
+entrada = 1234
+
+a = entrada % 10
+b = entrada % 100
+
+print(b)
